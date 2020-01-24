1091c4a906969d1989e9cab4c2eb7d73b37552b7
McAtomica <60275856+McAtomica@users.noreply.github.com>
2020-01-24 23:36:44 +0100

    Plik główny.

---
+++ Main.c
@@ -0,0 +1,9 @@
+#include <stdio.h> //operacje I/O
+#include <string.h> //Funkcja strlen()
+#include <stdlib.h> //funkcja atoi()
+#include <conio.h> // funkcja getch()
+
+int main(){
+    printf("Silnia [...]");
+    return 0;
+}
